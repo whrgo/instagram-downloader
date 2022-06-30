515327a0d43fcb301073cc438bc484738cb5e9cb
whrgo <dev.whrgo@gmail.com>
2022-06-30 18:04:12 +0900

    simple downloader

--- Main.py
+++ Main.py
@@ -6,22 +6,34 @@ class Frame(InstagramDownloader):
     def __init__(self, parent, title):
         super(Frame, self).__init__(parent, title=title)
 
-        self.__do_Bind()
+        self.__do_bind()
 
         self.Show()
         self.Centre()
     
-    def __do_Bind(self):
+    def __do_bind(self):
         self.btnDownload.Bind(wx.EVT_BUTTON, self.DownloadStart)
         self.btnDownloadStop.Bind(wx.EVT_BUTTON, self.DownloadStop)
 
-    def DownloadStart(self, event):
+    def DownloadStart(self, event=None):
         self.btnDownload.Disable()
         self.btnDownloadStop.Enable()
+        try: 
+            instance = Downloader.Downloader(self.ID.GetValue())
+            instance.downloadPosts()
 
-    def DownloadStop(self, event):
+        except Exception as e:
+            print("Error", e, wx.ICON_WARNING)
+            return
+
+    def DownloadStop(self, event=None):
         self.btnDownload.Enable()
         self.btnDownloadStop.Disable()
+
+
+
+    def print(self, title, message, props=None):
+        wx.MessageBox(title,  message, props)
 
 
 if __name__ == '__main__':
